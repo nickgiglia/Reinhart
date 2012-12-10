8d63b43d78441bd7554b3a42dff45813900fa944
Nick Giglia <njgiglia@gmail.com>
2012-12-10 15:43:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reinhart::Application.config.secret_token = 'e06adc5538588d6dbe239eba9ff24c7e86ecfe052d421898ef7d12bbcd5e9642102cdc609ae62f8d050ab79bebc4516083919fad8f33e7fe242a38adbabffd21'
